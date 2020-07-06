21748e7a6abc40541cfb087b88c5fd9a46b3f523
Tom <tomm@brandwatch.com>
2020-07-06 10:31:54 +0100

    home page and navigation

--- src/templates/componentTemplate.js
+++ src/templates/componentTemplate.js
@@ -3,6 +3,9 @@ import { graphql } from "gatsby"
 import PageHeading from "../axiom-docs/PageHeading"
 import SectionTitle from "../axiom-docs/SectionTitle"
 import ImageWithCopy from "../axiom-docs/ImageWithCopy"
+import SideNav from "../components/SideNav"
+
+import "./componenttemplate.css"
 
 export default function ComponentTemplate({
   data, // this prop will be injected by the GraphQL query below.
@@ -10,22 +13,32 @@ export default function ComponentTemplate({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter } = markdownRemark
   console.log("frontmatter", frontmatter)
+  const documentNames = data.allMarkdownRemark.edges.map(edge => {
+    return edge.node.frontmatter.component_name
+  })
+  console.log(documentNames)
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <PageHeading
-          title={frontmatter.component_name}
-          content={frontmatter.introduction}
-        />
-        {Object.entries(frontmatter.section).map(([key, value]) => {
-          if (!value) return null
-          return (
-            <div>
-              <SectionTitle title={value.title} content={value.introduction} />
-              <ImageWithCopy img={value.image} content={value.introduction} />
-            </div>
-          )
-        })}
+    <div className="template-container ax-theme--day">
+      <SideNav documents={documentNames} />
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <PageHeading
+            title={frontmatter.component_name}
+            content={frontmatter.introduction}
+          />
+          {Object.entries(frontmatter.section).map(([key, value]) => {
+            if (!value) return null
+            return (
+              <div>
+                <SectionTitle
+                  title={value.title}
+                  content={value.introduction}
+                />
+                <ImageWithCopy img={value.image} content={value.introduction} />
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
@@ -33,6 +46,15 @@ export default function ComponentTemplate({
 
 export const pageQuery = graphql`
   query($component_name: String!) {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            component_name
+          }
+        }
+      }
+    }
     markdownRemark(frontmatter: { component_name: { eq: $component_name } }) {
       frontmatter {
         title
@@ -60,3 +82,17 @@ export const pageQuery = graphql`
     }
   }
 `
+
+// export const tomm = graphql`
+//   query tomm {
+//     allMarkdownRemark {
+//       edges {
+//         node {
+//           frontmatter {
+//             component_name
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
