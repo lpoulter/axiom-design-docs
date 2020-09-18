eec12e1960b144a4078371b85a6bf63675083126
laurie <laurie@brandwatch.com>
2020-09-18 14:56:11 +0100

    Make search work

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,23 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
-import { Base, TextInput, TextInputIcon } from "@brandwatch/axiom-components"
+import { TextInput, TextInputIcon } from "@brandwatch/axiom-components"
 
 import "./Sidebar.css"
 
 function Sidebar({ componentNames }) {
+  const [filterString, setFilterString] = useState("")
+  const filterComponents = componentNames.filter(name =>
+    name.toLowerCase().includes(filterString.toLowerCase())
+  )
+
   return (
     <div className="sidebar">
       <div className="sidebar__search-container">
         <TextInput
           aria-label="Search components"
           placeholder="Search components"
+          value={filterString}
+          onChange={e => setFilterString(e.target.value)}
         >
           <TextInputIcon align="left" name="magnify-glass" iconColor="subtle" />
         </TextInput>
       </div>
 
       <ul className="sidebar__links">
-        {componentNames.map(componentName => {
+        {filterComponents.map(componentName => {
           return (
             <li key={componentName}>
               <Link
