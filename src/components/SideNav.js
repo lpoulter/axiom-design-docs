f324daa058559ea711266437b56b99ae2d9d751e
laurie <laurie@brandwatch.com>
2020-09-18 13:30:29 +0100

    Style search compnent search

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
-
-import { Base, Heading, TextInput, Strong } from "@brandwatch/axiom-components"
+import { Base, TextInput, TextInputIcon } from "@brandwatch/axiom-components"
 
 import "./sidenav.css"
 
 const SideNav = ({ documents }) => {
   return (
     <div className="sideNav">
-      <TextInput placeholder="search components" />
+      <TextInput aria-label="Search components" placeholder="Search components">
+        <TextInputIcon align="left" name="magnify-glass" iconColor="subtle" />
+      </TextInput>
       <ul>
         {documents.map(doc => {
           return (
